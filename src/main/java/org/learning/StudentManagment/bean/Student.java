7eccab63f5415d26389463518b8daeeddb19e2f2
vinod <maddelavinod1730@gmail.com>
2025-08-03 16:58:00 -0500

    commited second time to test how to add

--- src/main/java/org/learning/StudentManagment/bean/Student.java
+++ src/main/java/org/learning/StudentManagment/bean/Student.java
@@ -3,5 +3,6 @@ package org.learning.StudentManagment.bean;
 public class Student {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Vinod");
     }
 }
